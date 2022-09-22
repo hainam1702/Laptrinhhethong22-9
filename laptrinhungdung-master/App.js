b427a8f883b9db4d4ced93fc50d0ed08a5b7615c
hainam1702 <112164979+hainam1702@users.noreply.github.com>
2022-09-22 14:27:43 +0700

    first commit

---
+++ laptrinhungdung-master/App.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/pages/Home';
+import Register from './src/pages/Register';
+import Login from './src/pages/Login';
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
